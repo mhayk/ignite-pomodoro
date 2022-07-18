719dbade7740c6aeec120e7273896ad7b16f9776
Mhayk Whandson <hi@mhayk.com>
2022-07-18 10:17:42 +0100

    feat: styled-components has been configured

---
+++ src/components/Button.tsx
@@ -0,0 +1,10 @@
+import { ButtonContainer, ButtonVariant } from "./Button.styles"
+
+interface ButtonProps {
+    variant?: ButtonVariant;
+}
+
+export function Button({ variant: color = 'primary' }: ButtonProps) {
+    return <ButtonContainer variant={color}>Send</ButtonContainer>
+}
+
