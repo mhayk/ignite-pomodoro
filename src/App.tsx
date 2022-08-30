359ea8b0d274c09a60e39dcd1fdec61df894fc1a
Mhayk Whandson <hi@mhayk.com>
2022-08-30 23:29:22 +0100

    route component has been added.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
 import { ThemeProvider } from 'styled-components'
-import { Button } from './components/Button'
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
 import { defaultTheme } from './styles/themes/default'
 
 export function App() {
 
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary" />
-      <Button variant='secondary' />
-      <Button variant='danger' />
-      <Button variant='success' />
-      <Button />
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
     </ThemeProvider>
   )
 }
