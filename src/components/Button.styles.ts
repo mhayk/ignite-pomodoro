1ff0fed0e2d32877f367e02cece80aff66d15faa
Mhayk Whandson <hi@mhayk.com>
2022-09-02 10:30:17 +0100

    feat: add colors style

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -1,29 +1,28 @@
-import styled, { css } from 'styled-components';
+import styled, { css } from "styled-components";
 
-export type ButtonVariant = 'primary' | 'secondary' | 'danger' | 'success';
+export type ButtonVariant = "primary" | "secondary" | "danger" | "success";
 
 interface ButtonContainerProps {
-    variant: ButtonVariant;
+  variant: ButtonVariant;
 }
 
 const buttonVariants = {
-    primary: 'purple',
-    secondary: 'orange',
-    danger: 'red',
-    success: 'green',
-}
+  primary: "purple",
+  secondary: "orange",
+  danger: "red",
+  success: "green",
+};
 
 export const ButtonContainer = styled.button<ButtonContainerProps>`
-    width: 100px;
-    height: 40px;
+  width: 100px;
+  height: 40px;
 
-    background-color: ${props => props.theme.primary};
-    color: ${props => props.theme.white};
+  background-color: ${(props) => props.theme["green-500"]};
+  color: ${(props) => props.theme.white};
 
-    border: none;
+  border: none;
 
-    /* ${({ variant }) => css`
-        background-color: ${buttonVariants[variant]};
-    `} */
-
-`
+  /* ${({ variant }) => css`
+    background-color: ${buttonVariants[variant]};
+  `} */
+`;
