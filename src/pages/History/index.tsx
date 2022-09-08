fa6babc2f610d1c2a91b54fa9c7a805270ce0719
Mhayk Whandson <hi@mhayk.com>
2022-09-08 13:56:48 +0100

    style:

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,4 +1,4 @@
-import { HistoryContainer, HistoryList } from "./styles";
+import { HistoryContainer, HistoryList, Status } from "./styles";
 
 export function History() {
   return (
@@ -20,43 +20,57 @@ export function History() {
               <td>Task</td>
               <td>20 minutes</td>
               <td>Around 2 months</td>
-              <td>Finished</td>
+              <td>
+                <Status statusColor="green">Finished</Status>
+              </td>
             </tr>
             <tr>
               <td>Task</td>
               <td>20 minutes</td>
               <td>Around 2 months</td>
-              <td>Finished</td>
+              <td>
+                <Status statusColor="green">Finished</Status>
+              </td>
             </tr>
             <tr>
               <td>Task</td>
               <td>20 minutes</td>
               <td>Around 2 months</td>
-              <td>Finished</td>
+              <td>
+                <Status statusColor="green">Finished</Status>
+              </td>
             </tr>
             <tr>
               <td>Task</td>
               <td>20 minutes</td>
               <td>Around 2 months</td>
-              <td>Finished</td>
+              <td>
+                <Status statusColor="green">Finished</Status>
+              </td>
             </tr>
             <tr>
               <td>Task</td>
               <td>20 minutes</td>
               <td>Around 2 months</td>
-              <td>Finished</td>
+              <td>
+                <Status statusColor="red">Finished</Status>
+              </td>
             </tr>
             <tr>
               <td>Task</td>
               <td>20 minutes</td>
               <td>Around 2 months</td>
-              <td>Finished</td>
+              <td>
+                <Status statusColor="yellow">Finished</Status>
+              </td>
             </tr>
             <tr>
               <td>Task</td>
               <td>20 minutes</td>
               <td>Around 2 months</td>
-              <td>Finished</td>
+              <td>
+                <Status statusColor="green">Finished</Status>
+              </td>
             </tr>
           </tbody>
         </table>
